2a8769ed117ccb64318cfba7215ce46d1c1ba7a9
Kelvy Lima <kelvy2@gmail.com>
2022-08-05 15:21:01 -0300

    exercise 5 to 7 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(initnum, finalnum) {
+    let sum = 0;
+    if(initnum < 0 || typeof(initnum) !== 'number' || typeof(finalnum) !== 'number') {
+        return 'ERROR'
+    } else {
+        if(initnum > finalnum) {
+            for(let i = initnum; i >= finalnum; i--){
+                sum+=i
+            };
+        } else {
+            for(let i = initnum; i <= finalnum; i++) {
+            sum += i
+            };
+        };
+        return sum;}
 };
-
+console.log(sumAll(4, 1))
+console.log(typeof(90))
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,16 @@
-const ftoc = function() {
+const ftoc = function(ftemp) {
+    let c = (ftemp-32)*(5/9)
+    return (Math.round(c*10)/10)
 
 };
 
-const ctof = function() {
-
+const ctof = function(ctemp) {
+    let f = (ctemp*(9/5))+32
+    return (Math.round(f*10)/10)
 };
 
+console.log(ftoc(0))
+console.log(ctof(1))
 // Do not edit below this line
 module.exports = {
   ftoc,
